4e5dc9bedfb2e5c4ecbaed17edcb8b08e7daf99e
Chandan <chandan.kumar@oodlestechnologies.com>
2017-10-05 15:33:29 +0530

    renamed test file name

--- src/test/java/com/bolenum/coreservice/EthCoreServiceApplicationTests.java
+++ src/test/java/com/bolenum/coreservice/EthCoreServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.oodles.wallettest;
+package com.bolenum.coreservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WalletTestApplicationTests {
+public class EthCoreServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
